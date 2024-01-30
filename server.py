814b173f5d3a9fcbace4470a4661a1e47a7849d6
Jack Honour <JaxkDev@gmail.com>
2024-01-30 21:16:41 +0000

    Upload working

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from os.path import join as pjoin
+from os import mkdir
+from os.path import exists, join as pjoin
 from urllib.parse import parse_qs, urlparse
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
@@ -6,10 +7,14 @@ def getDirectory():
     from inspect import getsourcefile
     from os.path import abspath
 
-    return abspath(getsourcefile(lambda:0))
+    return abspath(getsourcefile(lambda:0) + "/../")
 
 class StoreHandler(BaseHTTPRequestHandler):
     directory = pjoin(getDirectory(), 'Data')
+    if(not exists(directory)):
+        mkdir(directory)
+    if(not exists(pjoin(directory, 'Raw'))):
+        mkdir(pjoin(directory, 'Raw'))
 
     def do_GET(self):
         self.path = self.path.lower()
@@ -75,22 +80,24 @@ class StoreHandler(BaseHTTPRequestHandler):
         length = self.headers['content-length']
         data = self.rfile.read(int(length))
 
-        with open(pjoin(self.directory, 'raw', uuid), 'w') as fh:
-            fh.write(data.decode())
+        with open(pjoin(self.directory, 'raw', uuid + '.mp4'), 'wb') as fh:
+            fh.write(data)
 
         self.send_response(200)
         self.end_headers()
         
+        print("New analysis video uploaded: " + uuid)
         #TODO, Queue for processing
         return;
 
+    # Allows client to download original video.
     def get_download(self, uuid):
         self.send_response(200)
         self.send_header('content-type', 'video/mp4')
         self.end_headers()
 
-        with open(pjoin(self.directory, 'raw', uuid), 'r') as fh:
-            self.wfile.write(fh.read().encode())
+        with open(pjoin(self.directory, 'raw', uuid + '.mp4'), 'rb') as fh:
+            self.wfile.write(fh.read())
     
     def get_status(self, uuid):
         self.send_response(200)
